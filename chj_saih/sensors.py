f0e0999da81410296cacaad0732c3848e967125f
carlos-48 <63410909+carlos-48@users.noreply.github.com>
2024-11-04 23:47:15 +0100

    Update sensors.py
    Incluyo el parseo dentro de cada sensor. Esta hecho por IA y sin probar, pues estoy desde el móvil y no puedo probar que responde cada consulta. Pendiente de ajustar cuando pueda

--- chj_saih/sensors.py
+++ chj_saih/sensors.py
@@ -1,3 +1,5 @@
+import datetime
+
 class Sensor:
     def __init__(self, variable, period_grouping, num_values):
         self.variable = variable
@@ -6,20 +8,88 @@ class Sensor:
 
     def get_data(self):
         from chj_saih.data_fetcher import fetch_sensor_data
-        return fetch_sensor_data(self.variable, self.period_grouping, self.num_values)
+        raw_data = fetch_sensor_data(self.variable, self.period_grouping, self.num_values)
+        return self.parse_data(raw_data) if raw_data else None
+
+    def parse_data(self, raw_data):
+        """
+        Método base para parsear los datos en un formato estándar.
+        Deberá ser sobrescrito por cada subclase.
+        """
+        raise NotImplementedError("Este método debe ser implementado por subclases.")
 
 class RainGaugeSensor(Sensor):
     """Sensor para medir la lluvia (pluviómetro)."""
-    pass
+
+    def parse_data(self, raw_data):
+        values = list(
+            filter(
+                lambda dv: dv[1] is not None,
+                map(
+                    lambda dv: (
+                        datetime.datetime.strptime(dv[0], '%d/%m/%Y %H:%M'),
+                        dv[1]
+                    ),
+                    raw_data[1]  # Asume que el JSON contiene los valores en raw_data[1]
+                )
+            )
+        )
+        values.sort(key=lambda x: x[0])  # Ordena por fecha
+        return {"rainfall_data": values}
 
 class FlowSensor(Sensor):
     """Sensor para medir el caudal de un río (aforo)."""
-    pass
+
+    def parse_data(self, raw_data):
+        values = list(
+            filter(
+                lambda dv: dv[1] is not None,
+                map(
+                    lambda dv: (
+                        datetime.datetime.strptime(dv[0], '%d/%m/%Y %H:%M'),
+                        dv[1]
+                    ),
+                    raw_data[1]
+                )
+            )
+        )
+        values.sort(key=lambda x: x[0])
+        return {"flow_data": values}
 
 class ReservoirSensor(Sensor):
     """Sensor para medir la cantidad de agua almacenada en un embalse."""
-    pass
+
+    def parse_data(self, raw_data):
+        values = list(
+            filter(
+                lambda dv: dv[1] is not None,
+                map(
+                    lambda dv: (
+                        datetime.datetime.strptime(dv[0], '%d/%m/%Y %H:%M'),
+                        dv[1]
+                    ),
+                    raw_data[1]
+                )
+            )
+        )
+        values.sort(key=lambda x: x[0])
+        return {"reservoir_data": values}
 
 class TemperatureSensor(Sensor):
     """Sensor para medir la temperatura ambiental."""
-    pass
+
+    def parse_data(self, raw_data):
+        values = list(
+            filter(
+                lambda dv: dv[1] is not None,
+                map(
+                    lambda dv: (
+                        datetime.datetime.strptime(dv[0], '%d/%m/%Y %H:%M'),
+                        dv[1]
+                    ),
+                    raw_data[1]
+                )
+            )
+        )
+        values.sort(key=lambda x: x[0])
+        return {"temperature_data": values}
