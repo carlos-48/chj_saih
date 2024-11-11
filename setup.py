d4727dddef3643e3ca0f55d39f2e85a4e6992eb7
carlos-48 <63410909+carlos-48@users.noreply.github.com>
2024-11-11 20:33:04 +0100

    Revert setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="chj-saih",
+    name="chj_saih",
     version="0.1",
     packages=find_packages(),
     install_requires=["requests"],
