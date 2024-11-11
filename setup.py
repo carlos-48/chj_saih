747e88fee8c6132432e08a6a67c6a109d0336453
carlos-48 <63410909+carlos-48@users.noreply.github.com>
2024-11-11 20:24:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="chj_saih",
+    name="chj-saih",
     version="0.1",
     packages=find_packages(),
     install_requires=["requests"],
