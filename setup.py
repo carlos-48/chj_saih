8dcf0bec52c18610ecebd31ebe815361cc5d484a
carlos-48 <63410909+carlos-48@users.noreply.github.com>
2024-11-13 23:23:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,18 @@ from setuptools import setup, find_packages
 setup(
     name="chj_saih",
     version="0.1.beta",
+    author="carlos-48",
+    author_email="karloselmaster@gmail.com",
+    description="Interface to Confederación Hidrográfica del Júcar to get data from the Automatic Hydrologic Information Sistem (SAIH)",
+    long_description="file: README.md",
+    long_description_content_type="text/markdown",
+    url="https://github.com/carlos-48/chj_saih",
+    project_urls={
+        "Bug Reports": "https://github.com/carlos-48/chj_saih/issues"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
     packages=find_packages(),
     install_requires=["requests"],
     entry_points={
